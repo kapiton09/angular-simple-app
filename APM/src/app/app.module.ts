fa0a17d4f9a9c0b4e55519e83f82b160dc9c58d9
Kapil Shrestha <contact@kapilshrestha.com>
2017-10-02 16:18:44 +1300

    Product List Component Completed

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -3,13 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ProductListComponent
+    AppComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpacesPipe,
+    StarComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
